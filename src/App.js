52d48f0de352708042892299f90ec649ce67b039
HamiltonCarpenter <Hamilton.Carpenter.mail@gmail.com>
2022-12-06 17:13:26 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+  
+import Posts from "./components/Posts/Posts";
+import background from './resources/newworld.webp';
 
-function App() {
+  
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundImage: `url(${background})` }} className="main-container">
+      <h1 className="main-heading">
+        Bring Your Models To Life With Artisan Skill
+      </h1>
+
+      <p className="body">
+      Good day! My name is Benji, I have been a full time commission painter for 2 years. 
+      I initially got into painting playing Warhammer, but as my passion evolved i started getting more involved 
+      with the artistic aspect, I expanded my reach to other fantasy and sci-fi miniatures. I have been getting involved 
+      in painting competitions, and have even produced award winning work! Today, I paint armies for clients overseas and locally, extending my passion 
+      for gaming and painting to every miniature I paint.
+      </p>
+
+      <p className="body">
+      Contact: lordhunterwolf@gmail.com
+      </p>
+
+      <Posts />
+
+      <p className="body">
+      The price for each contract depends on the complexity of the model, the desired paint job, 
+      and the quanity and size of the figures to be painted. To get an idea of what your commision would cost, reach
+      out to me at: lordhunterwolf@gmail.com
+      </p>
+
     </div>
   );
-}
-
+};
+  
 export default App;
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Posts.css";
+import Post from "../Post/Post";
+import dreadImage from '../../resources/dreadnaut_frontal.jpg';
+import lessonImage from '../../resources/paintinglessons.jpeg';
+import ttsImage from '../../resources/ttsmodels.jpg';
+
+const Posts = () => {
+  const blogPosts = [
+    {
+      title: "Comissioning An Army",
+      body: `Assembling and painting models takes time and painstaking effort. 
+      You can save dozens of hours and represent your army with professional skill. 
+      Whether it's a hundred grunts, or one intracate centerpiece, Benji has you covered.`,
+      author: "Nishant Singh ",
+      imgUrl: dreadImage,
+        /*"/home/ham/Documents/NodeProjects/blog_site/resources/dreadnaut_frontal.jpg",*/
+    },
+    {
+      title: "Digital Artwork",
+      body: `A lot of modern wargamming happens on Tabletop Simulator. You can easily play games with people from 
+      around the world without ever leaving your house. The downside is that everyone's models look the same,   `,
+      author: "Monu Kr",
+      imgUrl:
+        ttsImage,
+    },
+    {
+      title: "Lessons ",
+      body: `You might prefer to learn how to paint better or how to work with digital modeling sofware. 
+      These skills will last a lifetime and provide you with a relaxing hobby. Luckily for you, Benji is 
+      a professional teacher in addition to being an award winning artist!`,
+      author: "Suresh Kr",
+      imgUrl:
+        lessonImage,
+    },
+    /*{
+      title: "Free Consultations",
+      body: `The price for each contract depends on the complexity of the model, the complexity of the desired paint job, 
+      as well as the quanity and size of the figures to be painted. To get an idea of when `, 
+      author: "Sonu Kr",
+      imgUrl:
+        "https://media.geeksforgeeks.org/img-practice/banner/cp-maths-java-thumbnail.png",
+    },*/
+  ];
+  
+  return (
+    <div className="posts-container">
+      {blogPosts.map((post, index) => (
+        <Post key={index} index={index} post={post} />
+      ))}
+    </div>
+  );
+};
+  
+export default Posts;
